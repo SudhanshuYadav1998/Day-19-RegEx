75b7dc1e0d7e5df9c8eaad373381b1f1963ce833
SudhanshuYadav1998 <sy7040@gmail.com>
2022-08-03 18:16:28 +0530

    UC-5 file Added

---
+++ Day-19-UC5/Day-19-UC5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+class Patterns
+{
+    public static string RegexRules = "^[a-zA-Z]{8}$";
+    public bool validateString(string sample)
+    {
+        return Regex.IsMatch(sample, RegexRules);
+    }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        Patterns patterns = new Patterns();
+        if (patterns.validateString("asdfghjk"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+            Console.WriteLine("Invalid");
+    }
+}
