4881fbdb4d994cdf5ae5fdc623ba758af9d2f084
SudhanshuYadav1998 <sy7040@gmail.com>
2022-08-03 18:10:01 +0530

    UC-1 file Added

---
+++ Day-19-UC1/Day-19-UC1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+class Patterns
+{
+    public static string Regex_First_Name = "^[A-Z][a-z]{2,}$";
+    public bool validateString(string sample)
+    {
+        return Regex.IsMatch(sample, Regex_First_Name);
+    }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        Patterns patterns = new Patterns();
+        if (patterns.validateString("Sudhanshu"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+            Console.WriteLine("Invalid");
+    }
+}
